9f3e9a357088bb5a84134d0c14b5bd2b303c925f
Koina Ian <koinaobedian@gmail.com>
2025-04-27 15:04:06 +0300

    Updated styles and content for Boresha Maisha Initiative website

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth Scrolling for anchor links
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        // Prevent default behavior
+        e.preventDefault();
+
+        // Scroll to the section related to the link clicked
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',  // Scroll smoothly
+            block: 'start'       // Align the element to the top of the viewport
+        });
+    });
+});
